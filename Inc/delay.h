a5d38b95d607576f1bd9860ff3df0d145fb3b414
fuji <a1989796@yahoo.com.tw>
2017-05-19 00:52:30 +0800

    change delay function to macro function

--- Inc/delay.h
+++ Inc/delay.h
@@ -1,9 +1,7 @@
 #ifndef __DELAY_H
 #define __DELAY_H
-#include "stm32f4xx.h"
 
-void SysTick_Init(void);
-void TimeTick_Decrement(void);
-void delay(uint32_t n);
+#define delay(ms) \
+ (osDelay(ms));\
 
 #endif
